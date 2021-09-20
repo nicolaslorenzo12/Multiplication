385d6e56be5b020404581afaab7ec5cc7a97ad51
Nicolas Lorenzo <nicolaslorenzohermosilla@gmail.com>
2021-09-20 15:30:43 +0200

    Multiplication code

---
+++ src/multiplication/Multiplication.java
@@ -0,0 +1,29 @@
+package multiplication;
+import java.util.Scanner;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        int numberToMultiply;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Which multiplication table would you like to see?: ");
+        numberToMultiply = keyboard.nextInt();
+        int answer;
+        int correct = 0;
+        int wrong = 0;
+        for(int i = 1; i <= 10; i++){
+
+            System.out.print(i + " * " + numberToMultiply + "= " );
+            answer = keyboard.nextInt();
+
+            if(answer == i * numberToMultiply){
+                System.out.println("Correct");
+                correct ++;
+            }
+            else{
+                System.out.println("Wrong!");
+                wrong ++;
+            }
+        }
+        System.out.println("You had " + correct + " questions and " + wrong + " wrong questions");
+    }
+}
